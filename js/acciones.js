ee8aac8fa58eea9616bb6f78965644009d35f7e8
Marii2340 <maria.postrero.1g@gmail.com>
2018-05-23 11:18:42 -0500

    3subida

--- js/acciones.js
+++ js/acciones.js
@@ -1,26 +1,22 @@
 // JavaScript Document
-
 $(document).ready(function(e) {
-    document .addEventListener("deviceready",function(){
-		
+    document.addEventListener("deviceready",function(){
 		$('#izquierda').on("swipeleft",function(){
-			navigator .notification. alert("Deslizo a la izaquierda",function(){"Aplicacion7","Aceptar"});
-});
-
-$('#derecha').on("swiperight",function(){
-	navigator .notification. confirm("¿Que quieres hacer?",function(opt){
-		switch(opt)
-		{
-			case 1:
-			navigator.notification.beep(1);
-			break;
-			
-			case 2:
-			navigator.notification.vibrate(1000);
-			break;
-		}
-	},"Aplicacion7","Beep,Vibrar,Cancelar");
-	
-});
+			navigator.notification.alert("Deslizó a la izquierda",function(){"Aplicacion 7","OK"});
+		});
+		$('#derecha').on("swiperight",function(){
+			navigator.notification.confirm("¿Que deseas hacer?",function(op){
+				switch(op)
+				{
+					case 1:
+					navigator.notification.beep(1);
+				    break;
+					case 2:
+					navigator.notification.vibrate(1500);
+					break;			
+				}
+			},"Aplicacion 7","Sonar,vibrar,cancelar");
+		});
 	},false);
 });
+	
